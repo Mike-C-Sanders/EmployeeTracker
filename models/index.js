af026ac820f31daec29ca00d8a32bf1b9c9c1763
Mike Sanders <msanders331@gmail.com>
2022-03-14 20:46:02 -0700

    added the associations for all models under the index file found in the models folder.

--- models/index.js
+++ models/index.js
@@ -1,1 +1,32 @@
 //Index file used to create the relationships between all of the tables outlined in the other model files
+const Department = require('./Department');
+const Role = require('./Role');
+const Employee = require('./Employee');
+
+Department.hasMany(Role, {
+    foreignKey: 'department_id',
+    onDelete: 'CASCADE',
+});
+
+Role.hasMany(Employee, {
+    foreignKey: 'role_id',
+    onDelete: 'CASCADE',
+});
+
+Employee.hasOne(Employee, {
+    foreignKey: 'manager_id',
+});
+
+Role.belongsTo(Department, {
+    foreignKey: 'department_id'
+});
+
+Employee.belongsTo(Role, {
+    foreignKey: 'role_id',
+})
+
+Employee.belongsTo(Employee, {
+    foreignKey: 'manager_id'
+});
+
+module.exports = {Department, Role, Employee};
