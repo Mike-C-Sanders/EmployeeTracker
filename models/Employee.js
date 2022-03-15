977855e98580e7bff8cc91d2866e0fd5b1d91a3e
Mike Sanders <msanders331@gmail.com>
2022-03-14 20:25:03 -0700

    added Employee model. Next putting it together and adding all of the associations with the Index.js file found in the models folder.

---
+++ models/Employee.js
@@ -0,0 +1,54 @@
+//The file creates the Employee model
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Employee extends Model{}
+
+Employee.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        //employee first name max 30 characters
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlphanumeric: true,
+                len: [1, 30],
+            }
+        },
+        //employee last name max 30 characters
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlphanumeric: true,
+                len: [1, 30],
+            }
+        },
+        //role ID references the employee role
+        role_id:{
+            type: DataTypes.INTEGER,
+            references:{
+                model: 'role',
+                key: 'id',
+            },
+        },
+        //manager ID hold reference to another employee aka the manager
+        manager_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'employee',
+                key: 'id',
+            }
+
+        },
+    },
+);
+
+//export the employee model
+module.exports = Employee;
---
+++ models/index.js
@@ -0,0 +1,1 @@
+//Index file used to create the relationships between all of the tables outlined in the other model files
